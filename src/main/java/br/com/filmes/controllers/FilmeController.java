8b5db4707ae284f290f0a23e92760f0f3e90645d
TadeunASCIImento <tadeunascimento36@gmail.com>
2023-02-24 19:53:42 -0300

    [Refactor] Refaturando metodo home para logar erro.

--- src/main/java/br/com/filmes/controllers/FilmeController.java
+++ src/main/java/br/com/filmes/controllers/FilmeController.java
@@ -4,30 +4,42 @@ import static br.com.filmes.utils.constants.FilmeControllerConstants.TOTAL_PAGE_
 
 import java.util.List;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonParseException;
 
 import br.com.filmes.models.Filme;
 import br.com.filmes.models.Filmes;
 import br.com.filmes.services.FilmeService;
+import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@Slf4j
+@RestController
 public class FilmeController {
 
 	@Autowired
 	private FilmeService service;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView home(Integer pagina) {
+	@RequestMapping(method = GET, value = "/", produces = APPLICATION_JSON_VALUE)
+	public ModelAndView home(Integer pagina) throws JsonParseException {
 		ModelAndView mdv = new ModelAndView("index");
-		List<Filme> filmes = service.getPoster(service.getFilmes(pagina).getResults());
-		mdv.addObject("filmes", filmes);
-		mdv.addObject("totalPaginas", service.getFilmes(pagina).getTotal_pages());
-		mdv.addObject("anoAtual", service.getDataAtual());
+		try {
+			List<Filme> filmes = service.getPoster(service.getFilmes(pagina).getResults());
+			mdv.addObject("filmes", filmes);
+			mdv.addObject("totalPaginas", service.getFilmes(pagina).getTotal_pages());
+			mdv.addObject("anoAtual", service.getDataAtual());
+		} catch (Exception e) {
+			log.error("Error to load home page.", e.getMessage(), e);
+		}
 		return mdv;
 	}
 
